b18c7c5458b4059bfdf5f7b898f9211beb02deb2
Kamil Smolinski <kamil.smolinski94@gmail.com>
2024-07-06 15:55:54 +0200

    feature/CI-CD
    vue config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-      ? '/Kamil496/kamil-smolinski/tree/gh-pages/'
+      ? '/kamil-smolinski/'
       : '/',
 
   outputDir: 'dist',
